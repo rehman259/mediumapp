20a29947e5857b7129f0d6e13fed4fc9ccfe38e9
rehman259 <rehman259@gmail.com>
2019-01-29 17:33:16 +0500

    uncomment the seeder calls

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,9 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-       /* $this->call(UsersTableSeeder::class);
+       // $this->call(UsersTableSeeder::class);
         $this->call(SettingsTableSeeder::class);
-        $this->call(CategoryTableSeeder::class); */
+        $this->call(CategoryTableSeeder::class);
         $this->call(TagTableSeeder::class);
     }
 
